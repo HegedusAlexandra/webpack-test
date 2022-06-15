fd9fe1226f5696c0c4f5bdb945ba56d9ee9c22a4
HegedusAlexandra <hexapastry@gmail.com>
2022-06-15 10:44:11 +0200

    vége

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
+// Lodash, now imported by this script
 function component() {
     const element = document.createElement('div');
   
-    // Lodash, currently included via a script, is required for this line to work
+    
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
     return element;
